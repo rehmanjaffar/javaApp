545b123f7d1c1996b5616f1f65516b0d5c392b4f
rehmanjaffar <142385806+rehmanjaffar@users.noreply.github.com>
2023-08-22 15:05:06 +0500

    Create HelloWorldTest.java

---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import org.joda.time.LocalTime;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testCurrentTime() {
+        LocalTime currentTime = new LocalTime();
+        String expected = "The current local time is: " + currentTime;
+        String actual = HelloWorld.getCurrentTimeMessage(currentTime);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGreeter() {
+        Greeter greeter = new Greeter();
+        String expected = "DevOps Project for Systems Limited";
+        String actual = greeter.sayHello();
+        assertEquals(expected, actual);
+    }
+}
