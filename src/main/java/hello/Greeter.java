ad41e429ea3139931737b2754d32d91c42e0962e
rehmanjaffar <142385806+rehmanjaffar@users.noreply.github.com>
2023-09-08 22:32:42 +0500

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Systems Limited ";
+        return "DevOps Project for Systems Limited";
     }
 }
